914c20c39386edd883424e47d87f93a9b0e897c6
lesion <lesion@autistici.org>
2023-01-11 10:49:04 +0100

    vuetify theme and icons

--- plugins/2.vuetify.js
+++ plugins/2.vuetify.js
@@ -1,15 +1,48 @@
-// plugins/vuetify.js
-import { createVuetify } from 'vuetify'
+import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import '@mdi/font/css/materialdesignicons.css'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import colors from 'vuetify/lib/util/colors.mjs'
 
-
-export default defineNuxtPlugin(nuxtApp => {
+export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi,
+      },
+    },
+    theme: {
+      themes: {
+        light: {
+          dark: false,
+          colors: {
+            primary: colors.indigo.darken2,
+            secondary: colors.grey.darken3,
+            accent: colors.blue.accent1,
+            error: colors.red.accent2,
+            info: colors.blue.base,
+            success: colors.green.base,
+            warning: colors.red.darken1,
+          },
+        },
+        dark: {
+          dark: true,
+          colors: {
+            primary: colors.blue.darken4,
+            secondary: colors.grey.darken1,
+            accent: colors.pink.accent2,
+            error: colors.red.darken2,
+            info: colors.blue.base,
+            success: colors.green.darken2,
+            warning: colors.orange.darken1,
+          },
+        },
+      },
+    },
     components,
     directives,
-    // ssr: true
   })
 
   nuxtApp.vueApp.use(vuetify)
