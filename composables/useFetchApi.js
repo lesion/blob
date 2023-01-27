9621499c77c48087759bdb6faadbbe6302e42870
lesion <lesion@autistici.org>
2023-01-27 10:00:16 +0100

    admin

--- composables/useFetchApi.js
+++ composables/useFetchApi.js
@@ -1,11 +1,11 @@
-export default (url, options = {}) => {
-    const { useAuthToken } = useAuth()
+// export default (url, options = {}) => {
+//     const { useAuthToken } = useAuth()
 
-    return $fetch(url, {
-        ...options,
-        headers: {
-            ...options.headers,
-            Authorization: `Bearer ${useAuthToken().value}`
-        }
-    })
-}
+//     return $fetch(url, {
+//         ...options,
+//         headers: {
+//             ...options.headers,
+//             Authorization: `Bearer ${useAuthToken().value}`
+//         }
+//     })
+// }
---
+++ pages/admin.vue
@@ -0,0 +1,39 @@
+<script setup>
+const { $confirm } = useNuxtApp()
+
+const active = ref('settings')
+const about = ref('')
+
+</script>
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title>Admin</v-card-title>
+      
+      <v-tabs v-model='active'>
+        <v-tab value='settings'>{{$t('Settings')}}</v-tab>
+        <v-tab value='logs'>{{$t('Logs')}}</v-tab>
+      </v-tabs>
+      <v-card-text>
+        <v-window v-model='active'>
+          <v-window-item active value='settings'>
+            <v-row>
+              <v-col cols='8'>
+                <v-file-input label="Logo" variant='outlined'></v-file-input>
+              </v-col>
+              <v-col>
+                <v-img height='60' :src='`/blob.png`'></v-img>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-textarea variant='outlined' label='About' hint='A description of your website' persistent-hint v-model='about'></v-textarea>
+            </v-row>
+          </v-window-item>
+          <v-window-item value='logs'><AdminLogs></AdminLogs></v-window-item>
+
+        </v-window>
+
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
