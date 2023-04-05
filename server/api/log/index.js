0a3186bcbc23826b12466d1dea63a143375b2480
lesion <lesion@autistici.org>
2023-04-05 10:38:09 +0200

    order log by cration date desc

--- server/api/log/index.js
+++ server/api/log/index.js
@@ -4,6 +4,7 @@ import prisma from "~~/server/lib/db"
 export default defineEventHandler(async (event) => {
     return prisma.log.findMany({
         include: { source: true },
+        orderBy: [{ createdAt: 'desc' }],
         take: 100
     })
 })
