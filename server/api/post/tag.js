3ba89eaca9f6c6e8e9d2f187903dbe4617349ef9
lesion <lesion@autistici.org>
2023-09-13 10:18:35 +0200

    support pagination in tag api

--- server/api/post/tag.js
+++ server/api/post/tag.js
@@ -5,17 +5,30 @@ export default defineEventHandler(async (event) => {
   if (query.id) {
     const id = Number(query.id)
     const maxPosts = Number(query.maxPosts) || 10
+    const after = (query.after)
     
     const tag = await prisma.tag.findUnique({ where: { id }})
-    
+
     if (!tag) {
       return createError({ status: 404 })
     }
 
+    const where = { 
+      tags: { some: { id } },
+    }
+    
+    if (!event.context?.auth?.user) {
+      where['visible'] =  true
+    }
+    if (after) {
+      where['date'] = { lt: new Date(after) }
+    }
+
+
     const posts = await prisma.post.findMany({
       orderBy: [ { date: 'desc'} ],
       take: maxPosts,
-      where: { tags: { some: { id } } },
+      where,
       include: {
         tags: true,
         source: true
